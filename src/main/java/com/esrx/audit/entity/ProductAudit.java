6812848b3b3782320e66e54c785a3ea010dee864
saikumar <saikumarkatta971@gmail.com>
2025-07-25 11:10:08 +0530

    separated audit

---
+++ src/main/java/com/esrx/audit/entity/ProductAudit.java
@@ -0,0 +1,36 @@
+package com.esrx.audit.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public abstract class ProductAudit {
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
